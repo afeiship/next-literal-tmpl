1dd731fef1120e35ccc5d61b0a149b0b6ad48296
afeiship <1290657123@qq.com>
2021-07-20 12:58:55 +0800

    chore: to jswork

---
+++ src/index.js
@@ -0,0 +1,19 @@
+(function () {
+  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
+  var nx = global.nx || require('@jswork/next');
+  var FORMAT_RE = /\${(.*?)}/g;
+  var EMPTY_STR = '';
+
+  nx.literalTmpl = function (inString, inContext) {
+    var result = inString || EMPTY_STR;
+    var replaceFn = function (_, match) {
+      var path = match.trim();
+      return nx.get(inContext, path) || EMPTY_STR;
+    };
+    return result.replace(FORMAT_RE, replaceFn);
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.literalTmpl;
+  }
+})();
